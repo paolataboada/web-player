7372606aef09996946eec83825a7c73915ceda8d
Paola Taboada <taboadapaola3@gmail.com>
2025-10-21 16:59:28 -0500

    feat: remove local storage when log out

--- src/modules/home/pages/HomePage.tsx
+++ src/modules/home/pages/HomePage.tsx
@@ -1,13 +1,22 @@
 import { motion } from "framer-motion";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { ROUTES } from "../../../routes/routes";
 
 const HomePage = () => {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        navigate(ROUTES.LOGIN);
+        localStorage.removeItem("userToken");
+    }
+
     return (
         <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }}>
             <div className="flex justify-between items-center p-3">
                 <div>HomePage</div>
-                <Link to={ROUTES.LOGIN} className="bg-red-500 rounded-xl px-3 py-1">Log out</Link>
+                <button type="button" onClick={handleLogout} className="bg-red-500 rounded-xl px-3 py-1 cursor-pointer">
+                    Log out
+                </button>
             </div>
         </motion.div>
     )
