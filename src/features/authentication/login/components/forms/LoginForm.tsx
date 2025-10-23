2a7e9221d8b66e122c4280e7b84e3cd8f5c587c7
Paola Taboada <taboadapaola3@gmail.com>
2025-10-23 16:22:31 -0500

    feat: add button team maquet

--- src/features/authentication/login/components/forms/LoginForm.tsx
+++ src/features/authentication/login/components/forms/LoginForm.tsx
@@ -1,7 +1,38 @@
+import IconShirt from "@public/svg/shirt.svg";
+import IconCheck from "@public/svg/check.svg";
+import "../../../../../index.css";
+
 const LoginForm = () => {
     return (
-        <div>LoginForm</div>
-    )
-}
+        <div className="flex justify-center gap-4">
+            <div className="flex flex-col items-center gap-2">
+                <button
+                    className="w-full h-[100px] 
+                       rounded-tl-[20px] rounded-tr-[6px] rounded-br-[20px] border border-neutral-400 bg-neutral-900
+                       flex items-center justify-center"
+                >
+                    <img src={IconShirt} alt="Alianza Lima" className="w-16 h-16" />
+                </button>
+                <span className="font-body-normal-medium">Alianza Lima</span>
+            </div>
 
-export default LoginForm
+            <div className="flex flex-col items-center gap-2 relative">
+                <button
+                    className="w-full h-[100px] 
+                    rounded-tl-[20px] rounded-tr-[6px] rounded-br-[20px] rounded-bl-[6px]
+                    btn-gradient-border
+                    flex items-center justify-center
+                    relative
+                    custom-shadow"
+                >
+                    <img src={IconCheck} alt="Check" className="absolute -top-2 -right-2 w-5 h-5" />
+                    <img src={IconShirt} alt="Alianza Lima" className="w-16 h-16" />
+                </button>
+                <span className="font-body-normal-medium">Alianza Lima</span>
+            </div>
+
+        </div>
+    );
+};
+
+export default LoginForm;
