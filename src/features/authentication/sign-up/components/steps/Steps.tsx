b33f10a5592e70229b36bb391060b9a5f23269a9
GeorgeVillarroel <george.dan.villarroel@gmail.com>
2025-10-24 15:24:51 -0300

    feat: update StepIndicator

--- src/features/authentication/sign-up/components/steps/Steps.tsx
+++ src/features/authentication/sign-up/components/steps/Steps.tsx
@@ -1,48 +1,31 @@
+interface StepProps {
+    stepNumber: string,
+    stepText: string,
+    bgColor: {
+        barOne: string,
+        barTwo: string,
+        barThree: string,
+    },
+}
 
-export const StepOne = () => {
+interface StepObjProps {
+    stepObj: StepProps;
+}
+
+const Steps = ( {stepObj}: StepObjProps ) => {
     return (
         <div className="w-full max-w-[332px] lg:max-w-[426px] h-[87px] flex flex-col justify-between">
             <h3 className="h3 text-neutral-50 pt-[9px] h-8 text-center ">Crea tu cuenta</h3>
             <div className="h-[30px] flex flex-col p-0 m-0 justify-between">
                 <div className="steps h-1 p-0 m-0 flex justify-between gap-2">
-                    <div className="current-step w-[136px] h-1 rounded-sm bg-neutral-200"></div>
-                    <div className="next-step w-[136px] h-1 rounded-sm bg-neutral-500"></div>
-                    <div className="last-step w-[136px] h-1 rounded-sm bg-neutral-500" ></div>
+                    <div className={`w-[136px] h-1 rounded-sm transition-all duration-400 ${stepObj.bgColor.barOne}`}></div>
+                    <div className={`w-[136px] h-1 rounded-sm transition-all duration-400 ${stepObj.bgColor.barTwo}`}></div>
+                    <div className={`w-[136px] h-1 rounded-sm transition-all duration-400 ${stepObj.bgColor.barThree}`} ></div>
                 </div>
-                <p className="text-body-normal-regular text-neutral-200 h-[18px] text-center flex justify-center items-center ">Paso 1 de 3: Ingresa tus datos</p>
+                <p className="text-body-normal-regular text-neutral-200 h-[18px] text-center flex justify-center items-center ">Paso {stepObj.stepNumber} de 3: {stepObj.stepText}</p>
             </div>
         </div>
     )
 }
 
-export const StepTwo = () => {
-    return (
-        <div className="w-full max-w-[332px] lg:max-w-[426px] h-[87px] flex flex-col justify-between">
-            <h3 className="h3 text-neutral-50 pt-[9px] h-8 text-center ">Crea tu cuenta</h3>
-            <div className="h-[30px] flex flex-col p-0 m-0 justify-between">
-                <div className="steps h-1 p-0 m-0 flex justify-between gap-2">
-                    <div className="current-step w-[136px] h-1 rounded-sm bg-green-500"></div>
-                    <div className="next-step w-[136px] h-1 rounded-sm bg-neutral-200"></div>
-                    <div className="last-step w-[136px] h-1 rounded-sm bg-neutral-500" ></div>
-                </div>
-                <p className="text-body-normal-regular text-neutral-200 h-[18px] text-center flex justify-center items-center ">Paso 2 de 3: Personaliza tu cuenta</p>
-            </div>
-        </div>
-    )
-}
-
-export const StepThree = () => {
-    return (
-        <div className="w-full max-w-[332px] lg:max-w-[426px] h-[87px] flex flex-col justify-between">
-            <h3 className="h3 text-neutral-50 pt-[9px] h-8 text-center ">Crea tu cuenta</h3>
-            <div className="h-[30px] flex flex-col p-0 m-0 justify-between">
-                <div className="steps h-1 p-0 m-0 flex justify-between gap-2">
-                    <div className="current-step w-[136px] h-1 rounded-sm bg-green-500"></div>
-                    <div className="next-step w-[136px] h-1 rounded-sm bg-green-500"></div>
-                    <div className="last-step w-[136px] h-1 rounded-sm bg-neutral-200" ></div>
-                </div>
-                <p className="text-body-normal-regular text-neutral-200 h-[18px] text-center flex justify-center items-center ">Paso 3 de 3: Elige tu equipo favorito</p>
-            </div>
-        </div>
-    )
-}
+export default Steps
