2a9480ed1f68e27a18d7fe13b219c33c9170ebe7
Paola Taboada <taboadapaola3@gmail.com>
2025-10-27 13:38:29 -0500

    feat: add login service

--- src/features/authentication/login/services/api-login.service.ts
+++ src/features/authentication/login/services/api-login.service.ts
@@ -1,4 +1,25 @@
-export const apiLoginService = () => {
+import apiPublic from "@api/interceptors/api-public";
+import { setPlayer } from "@app/slices/player/player.slice";
+import type { AppDispatch } from "@app/store";
+import type { IPlayerJwtPayload, TRequestLogin, TResponseLogin } from "../types/api-login.types";
+import { jwtDecode } from "jwt-decode";
 
+type Params = (
+    dispatch: AppDispatch,
+    payload: TRequestLogin,
+) => Promise<TResponseLogin>;
+
+export const apiLoginService: Params = async (dispatch, payload) => {
+    const response = await apiPublic.post("/login", payload);
+
+    const token = response.data.data.token;
+    localStorage.setItem("token", token);
+
+    const decoded = jwtDecode<IPlayerJwtPayload>(token);
+    const player = decoded.player;
+    localStorage.setItem("player", JSON.stringify(player));
+
+    dispatch(setPlayer(player));
+
+    return response.data.data;
 }
-
