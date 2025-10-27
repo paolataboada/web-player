5c5cd0b866f114842d99e89ae2789749cde8ccaf
Alissonjackeline <alisorozcosilca1@gmail.com>
2025-10-27 10:42:55 -0500

    feat: CheckbostInput

--- src/features/authentication/sign-up/components/steps/custom-account/CustomAccount.tsx
+++ src/features/authentication/sign-up/components/steps/custom-account/CustomAccount.tsx
@@ -1,11 +1,27 @@
-import ColorPalette from "../../forms/ColorPalette"
+import AuthCheckboxInput from "../../../../shared/components/inputs/AuthChekbostInput";
+import ColorPalette from "../../forms/ColorPalette";
 
 const CustomAccount = () => {
-    return (
-        <div>
-            <ColorPalette />
-        </div>
-    )
-}
+  return (
+    <div>
+      <ColorPalette />
+      <AuthCheckboxInput
+        label="Declaracion"
+        linkText=""
+        href=""
+      />
+      <AuthCheckboxInput
+        label="Acepto recibir"
+        linkText="Informacion y Datos"
+        href="#"
+      />
+      <AuthCheckboxInput
+        label="Al hacer clic en siguiente acepta los"
+        linkText="Terminos y condiciones"
+        href="#"
+      />
+    </div>
+  );
+};
 
-export default CustomAccount
+export default CustomAccount;
