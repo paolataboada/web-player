b1f821f277568314b15bc99c551cdc506920dec8
Paola Taboada <taboadapaola3@gmail.com>
2025-11-07 15:01:38 -0500

    feat (ui styles): update golines icons

--- src/features/profile/components/cards/ReferredFriendCard.tsx
+++ src/features/profile/components/cards/ReferredFriendCard.tsx
@@ -1,35 +1,31 @@
-import Iconthree from "@global/assets/icons/main/three.svg";
+import IconGolines from "@global/assets/icons/main/golines.svg?react";
 import Fondogolines from "@global/assets/icons/main/Fondogolines.svg";
 
 export const ReferredFriendCard = () => {
   return (
     <div
-      className="w-[432px] h-[107px] rounded-3xl
+      className="w-[432px] h-[107px] rounded-2xl
             bg-linear-to-b from-secondary-600 via-primary-50 via-secondary-50 to-secondary-600 p-px "
     >
       <div
-        className="w-full h-full rounded-3xl
-            flex flex-row justify-between items-center bg-linear-to-b from-secondary-700 via-secondary-600 to-secondary-500 relative overflow-hidden"
+        className="w-full h-full rounded-2xl flex flex-row justify-between items-center relative overflow-hidden
+        bg-linear-[190deg] from-secondary-900 via-secondary-600 to-secondary-600"
       >
         <img
           className="h-full w-full absolute object-cover"
           src={Fondogolines}
           alt="fondo"
         />
-      
+
         <div className="flex-1 pl-4 z-10">
-          <p className="text-neutral-50 font-body-large-medium text-center">
+          <p className="font-body-large-medium text-center text-neutral-50 max-w-[280px]">
             ¡Llévate +50 golines por cada amigo que recomiendes!
           </p>
         </div>
 
-        <div className="z-20 h-full flex items-center">
-          <img
-            className="h-[107px]" style={{ mixBlendMode: "luminosity"}}
-            src={Iconthree}
-            alt="icon"
-          />
-        </div>
+        <IconGolines className="rotate-27 absolute top-10 right-4 w-[70px] h-[70px] mix-blend-luminosity" />
+        <IconGolines className="rotate-27 absolute -top-4 right-px w-[70px] h-[70px] mix-blend-luminosity" />
+        <IconGolines className="rotate-27 absolute top-0 right-[53.5px] w-[70px] h-[70px] mix-blend-luminosity" />
       </div>
     </div>
   );
